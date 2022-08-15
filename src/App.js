52b0b78b85f2d7db4f6261d54969a66aa5692005
Sakyuba Noe <398noe@gmail.com>
2022-08-15 20:44:08 +0900

    Challenge 06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 /**
@@ -8,11 +9,29 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-sussex/n02102480_4030.jpg");
+  const handleClick = async () => {
+    const newUrl = await fetchDogUrl();
+    setDogUrl(newUrl);
+  }
+  const fetchDogUrl = async () => {
+    return await fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          return result.message;
+        }
+      );
+  }
+
   return (
     <div>
       <header>Railway アプリ</header>
       <p>犬の画像を表示するサイトです。</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-sussex/n02102480_4030.jpg" />    
-      </div>
+      <img src={dogUrl} />
+      <br />
+      <button onClick={() => handleClick()}>更新</button>
+    </div>
   )
 }
+
